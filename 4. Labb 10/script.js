7c6788ac48b89ab99ad680fe075a0cba1874fdb0
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-19 17:04:18 +0200

    updated Labb 10

--- 4. Labb 10/script.js
+++ 4. Labb 10/script.js
@@ -1,21 +1,45 @@
+//KUBEN
+
 //Alternativ 1
 
 // const volume=function(width, length, height){
 //     return width * length * height;
 // }
 
-// console.log('Volymen på kvadraten är:' + volume(2, 5, 10));
+// console.log('Volymen på kuben är:' + volume(2, 5, 10));
 
 
 //Alternativ 2 - låter användaren fylla i
 
-let width= prompt('Skriv in bredden på kuben');
-let length = prompt('Skriv in längden på kuben')
-let height = prompt('Skriv in höjden på kuben')
+// let width= prompt('Skriv in bredden på kuben');
+// let length = prompt('Skriv in längden på kuben')
+// let height = prompt('Skriv in höjden på kuben')
 
-const volume=function(width, length, height){
-    return width * length * height;
+// const volume=function(width, length, height){
+//     return width * length * height;
+// }
+
+// alert('Volymen på kvadraten är:' + volume(width, length, height));
+// console.log('Volymen på kvadraten är:' + volume(width, length, height));
+
+
+//Cirkeln
+
+//Alternativ 1
+
+// const circleArea=function(diameter){
+//     return (diameter/2)*Math.PI;
+// }
+
+// console.log('Arean för cirkeln är:' + circleArea(20));
+
+// Alternativ 2 - låter användaren fylla i
+
+let diameter= prompt('Skriv in diametern på cirkeln');
+
+const circleArea=function(diameter){
+    return (diameter/2)*Math.PI;
 }
 
-console.log('Volymen på kvadraten är:' + volume(width, length, height));
-
+alert('Arean för cirkeln är:' + circleArea(diameter));
+console.log('Arean för cirkeln är:' + circleArea(diameter));
