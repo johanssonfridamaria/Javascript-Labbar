57a64ea802b2bab884711603203d294d3c9657e2
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-21 16:49:45 +0200

    working on the toggle menu

--- Labb 12/script.js
+++ Labb 12/script.js
@@ -2,5 +2,6 @@ let toogle = document.querySelector('#nav-toggle');
 let nav = document.querySelector('.navbar-nav');
 
 toogle.addEventListener('click', (e)=>{
-nav.classList.toogle(d-none);
+nav.classList.toggle('d-block');
+nav.classList.toggle('d-none');
 })
