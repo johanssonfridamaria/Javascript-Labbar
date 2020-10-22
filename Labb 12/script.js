8ed40cbf2505ad0de21eccef0081d76c17f01352
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-22 09:39:10 +0200

    changed page to start from desktop view, positioned elements in navbar with relative, absolute

--- Labb 12/script.js
+++ Labb 12/script.js
@@ -1,7 +1,7 @@
-let toogle = document.querySelector('#nav-toggle');
+let toggleButton = document.querySelector('#nav-toggle');
 let nav = document.querySelector('.navbar-nav');
 
-toogle.addEventListener('click', (e)=>{
+toggleButton.addEventListener('click', (e)=>{
 nav.classList.toggle('d-block');
 nav.classList.toggle('d-none');
 })
