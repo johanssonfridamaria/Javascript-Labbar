f22fa01bd33333707fad2bc144d5557d3432f739
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-22 13:44:33 +0200

    removed show class from toggle

--- Labb-12-from-scratch/script.js
+++ Labb-12-from-scratch/script.js
@@ -2,6 +2,5 @@ let toggleBtn=document.querySelector('#nav-toggle');
 let nav=document.querySelector('.navbar-nav');
 
 toggleBtn.addEventListener('click',(e)=>{
-    nav.classList.toggle('show');
     nav.classList.toggle('hide');
 });
