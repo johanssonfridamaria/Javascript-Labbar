e98826da70507cb086bfff804710842b84626534
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-22 12:09:47 +0200

    added hide and show class in mobile view to make to use for toggle

--- Labb-12-from-scratch/script.js
+++ Labb-12-from-scratch/script.js
@@ -2,6 +2,6 @@ let toggleBtn=document.querySelector('#nav-toggle');
 let nav=document.querySelector('.navbar-nav');
 
 toggleBtn.addEventListener('click',(e)=>{
-    nav.classList.toggle('d-flex');
-    nav.classList.toggle('d-none');
+    nav.classList.toggle('show');
+    nav.classList.toggle('hide');
 });
