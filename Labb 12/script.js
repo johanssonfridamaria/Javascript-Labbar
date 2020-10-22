9e721cc9715284b22284784096711bbec923e26c
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-22 10:09:59 +0200

    desktop view to mobile works. toggle is not ok

--- Labb 12/script.js
+++ Labb 12/script.js
@@ -2,6 +2,6 @@ let toggleButton = document.querySelector('#nav-toggle');
 let nav = document.querySelector('.navbar-nav');
 
 toggleButton.addEventListener('click', (e)=>{
-    nav.classList.toggle('d-none');
     nav.classList.toggle('d-block');
+    nav.classList.toggle('d-none');
 })
