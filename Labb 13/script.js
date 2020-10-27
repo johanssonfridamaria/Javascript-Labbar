23aead7ea789ff3834059e4f55cf48a0716575e9
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-27 09:26:55 +0100

    added validation function for inputs

--- Labb 13/script.js
+++ Labb 13/script.js
@@ -1,12 +1,37 @@
 const memberForm = document.querySelector('#memberForm');
-const firstName=document.querySelector('#firstName');
-const lastName=document.querySelector('#lastName');
-const email=document.querySelector('#email');
-const gender=document.querySelector('#gender');
-const dateOfBirth=document.querySelector('#dateOfBirth');
-const check=document.querySelector('#check');
-const btn=document.querySelector('#submit')
+const inputs = document.querySelectorAll('input');
 
-btn.addEventListener('submit', function(){
-    
+
+const email = document.querySelector('#email');
+const gender = document.querySelector('#gender');
+const dateOfBirth = document.querySelector('#dateOfBirth');
+const check = document.querySelector('#check');
+const btn = document.querySelector('#submit');
+
+
+
+let validate = function (id) {
+    let input = document.querySelector(id);
+    const error = document.querySelector(id + '-error');
+    if (input.value === '') {
+        error.innerText = 'Please enter a valid input.';
+        error.classList.add('text-danger')
+        input.classList.add('border-danger');
+    }
+    else if (input.value.length < 2) {
+        error.innerText = 'Please enter more than 2 characters';
+        error.classList.add('text-danger')
+        input.classList.add('border-danger');
+    }
+    else {
+        error.innerText = '';
+    }
+}
+
+
+
+
+
+btn.addEventListener('submit', function (e) {
+    e.preventDefault();
 })
