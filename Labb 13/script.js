65a0b570d7dab39d040efa2e4f31e241f43bb8b6
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-27 10:01:42 +0100

    added validation function for email

--- Labb 13/script.js
+++ Labb 13/script.js
@@ -1,37 +1,46 @@
 const memberForm = document.querySelector('#memberForm');
-const inputs = document.querySelectorAll('input');
+const email = document.querySelector('#email')
 
-
-const email = document.querySelector('#email');
-const gender = document.querySelector('#gender');
-const dateOfBirth = document.querySelector('#dateOfBirth');
-const check = document.querySelector('#check');
-const btn = document.querySelector('#submit');
-
-
-
-let validate = function (id) {
-    let input = document.querySelector(id);
+const validate = function (id) {
+    const input = document.querySelector(id);
     const error = document.querySelector(id + '-error');
     if (input.value === '') {
-        error.innerText = 'Please enter a valid input.';
-        error.classList.add('text-danger')
+        error.textContent = 'Please enter a valid input.';
+        error.classList.add('text-danger');
         input.classList.add('border-danger');
+        input.focus();
     }
     else if (input.value.length < 2) {
-        error.innerText = 'Please enter more than 2 characters';
-        error.classList.add('text-danger')
+        error.textContent = 'Please enter more than 2 characters';
+        error.classList.add('text-danger');
         input.classList.add('border-danger');
+        input.focus();
     }
     else {
-        error.innerText = '';
+        error.textContent = '';
     }
+}
+
+const validateEmail = function (email) {
+    const emailError = document.querySelector('#email-error')
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+        emailError.textContent = 'Please enter a valid email address';
+        emailError.classList.add('text-danger');
+        email.classList.add('border-danger');
+        email.focus();
+    }
+    else{
+        emailError.textContent ='';
+    }
+
 }
 
 
 
 
 
-btn.addEventListener('submit', function (e) {
+memberForm.addEventListener('submit', function (e) {
     e.preventDefault();
+    validate('#firstName');
+    validate('#lastName');
 })
