7ee7d0169e6642cfd8529bd04d76783c4daaa090
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-27 08:49:35 +0100

    added btn id

---
+++ Labb 13/script.js
@@ -0,0 +1,7 @@
+const memberForm = document.querySelector('#memberForm');
+const firstName=document.querySelector('#firstName');
+const lastName=document.querySelector('#lastName');
+const email=document.querySelector('#email');
+const gender=document.querySelector('#gender');
+const dateOfBirth=document.querySelector('#dateOfBirth');
+const check=document.querySelector('#check');
